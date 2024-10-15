8293c8f9b257250312ba736c1a57403439ea8199
amanlaiq <amoha211@uic.edu>
2024-10-15 18:42:58 -0500

    Added More Code

--- src/main.tsx
+++ src/main.tsx
@@ -5,3 +5,11 @@ import App from './App'
 import theme from './theme'
 import './index.css'
 
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
+)
