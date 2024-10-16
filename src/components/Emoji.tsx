5fcb28af9bc84848faa6bf8432ab00c72e3f962e
amanlaiq <amoha211@uic.edu>
2024-10-16 18:55:47 -0500

    New File

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,8 @@
+import bullsEye from '../assets/bulls-eye.webp';
+import thumbsUp from '../assets/thumbs-up.webp';
+import meh from '../assets/meh.webp';
+import { Image, ImageProps } from '@chakra-ui/react';
+
+interface Props {
+  rating: number;
+}
