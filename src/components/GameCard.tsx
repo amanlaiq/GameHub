656f7a81943f15ab81c5e1550e633100ae3e9211
amanlaiq <amoha211@uic.edu>
2024-10-22 13:19:02 -0500

    Added File

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,15 @@
+import { Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react'
+import React from 'react'
+import { Game } from '../hooks/useGames'
+import getCroppedImageUrl from '../services/image-url'
+import CriticScore from './CriticScore'
+import Emoji from './Emoji'
+import PlatformIconList from './PlatformIconList'
+
+interface Props {
+  game: Game
+}
+
+
+
+export default GameCard
