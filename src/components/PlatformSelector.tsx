c8bb728b2852a62a988d82e6e403791e6dca8272
amanlaiq <amoha211@uic.edu>
2024-10-23 11:49:07 -0500

    Added File

---
+++ src/components/PlatformSelector.tsx
@@ -0,0 +1,17 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { BsChevronDown } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
+import usePlatforms from "../hooks/usePlatforms";
+
+interface Props { 
+  onSelectPlatform: (platform: Platform) => void;
+  selectedPlatform: Platform | null;
+}
+
+const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
+  const { data, error } = usePlatforms();
+
+  if (error) return null;
+  
+  
+export default PlatformSelector;
